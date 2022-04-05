ea5ad41e9eb627e2561fa02dd3e2741c769a819d
Tuki1077 <70412089+Tuki1077@users.noreply.github.com>
2022-04-04 20:13:36 -0600

    Update

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const getData = {
     key: "LLJaKh9CU7LXwRsAIx7pR3dI2i7aIu48",
-    limit: 24,
+    limit: 12,
 };
 
 let gifs = [];
-const fetchPromise = fetch(`https://api.giphy.com/v1/gifs/trending?key=${getData.key}&limit=${getData.limit}`);
-fetchPromise.then(response => {
-    const jsonPromise = response.json();
-    jsonPromise.then(json => {
+const fecthURL = fetch(`https://api.giphy.com/v1/gifs/trending?key=${getData.key}&limit=${getData.limit}`);
+fecthURL.then(response => {
+    const URLPromise = response.json();
+    URLPromise.then(json => {
         const data = json.data;
         for (let i = 0; i < data.length; i++) {
             gifs.push(data[i]['embed_url']);
@@ -28,13 +28,13 @@ fetchPromise.then(response => {
     });
 });
 
-function reload(){
+function moreGifs(){
     ran = Math.floor((Math.random() * 1000) + 1);
     let gifs = [];
-    const fetchPromise = fetch(`https://api.giphy.com/v1/gifs/trending?key=${getData.key}&limit=12&offset=${ran}`);
-    fetchPromise.then(response => {
-    const jsonPromise = response.json();
-    jsonPromise.then(json => {
+    const fecthURL = fetch(`https://api.giphy.com/v1/gifs/trending?key=${getData.key}&limit=12&offset=${ran}`);
+    fecthURL.then(response => {
+    const URLPromise = response.json();
+    URLPromise.then(json => {
         const data = json.data;
         for (let i = 0; i < data.length; i++) {
             gifs.push(data[i]['embed_url']);
